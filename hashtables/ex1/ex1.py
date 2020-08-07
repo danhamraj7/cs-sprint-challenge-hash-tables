64e264844afa76f76c35d0ba656c64ac195c2469
danhamraj7 <ddan_7@hotmail.com>
2020-08-07 11:24:59 -0400

    tests pass for ex 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,27 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # create a dictionary of the weights
+    # returns a tuple
+    weights_dict = dict()
 
+    # iterate
+    for i in range(length):
+        # if a k:v pair is in the dictionary
+        # and is equal to the remainder
+        #  minus the curr ele from limit
+        x = weights_dict.get(limit - weights[i])
+        # if the value is found at the index in the dict
+
+        if x is not None:
+            # return tuple with the corresponding index
+            return (i, x)
+
+        else:
+            # if it is not found, set key as the curr
+            # val from list and val as the found index
+            weights_dict[weights[i]] = i
+
+    print(weights_dict)
+
+    # when the sum of the num in the list
+    # is not the limit, return none.
     return None
