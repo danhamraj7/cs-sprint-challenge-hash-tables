e3db727ae78c0ca2fb3f38bc6b9997399e7d2e7a
danhamraj7 <ddan_7@hotmail.com>
2020-08-07 13:51:34 -0400

    finish thought process for ex 3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -15,3 +15,17 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+    # thought process
+    # given a list that contains several sub-list of integers
+    # loop through all the sub-lists
+    # find all the integers that repeats itself in every single
+    # sublist.
+
+    # take the first ele in the first sub-list
+    # compare it to the ele in the second sub-list
+    # if it is not found go to the next ele in the first-sublist
+    # while the ele that is being compared is found in any sub list
+    # move to the other sub- list until the end.
+    # if it reaches the last sublist and the ele is found in all the
+    # sublist return the ele in the dict.
